286002120d0e7e2b2bdfced7dfa69075d626a5d4
Jolie Hobloss <jolieshobloss@gmail.com>
2021-01-17 15:27:17 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,30 @@
 // Code MovieReviews Here
+import React from 'react';
+
+function MovieReviews({ reviews }) {
+  return (<div className="review-list">
+    {reviews.map((el)=>{
+    return (<div
+      key={el.headline}
+      className="review"
+    >
+      <header>
+        <a
+          className="review-link"
+          href={el.link.url}
+        >
+          {el.headline}
+        </a>
+        <br/>
+        <span className="author">{el.byline}</span>
+      </header>
+      <blockquote>{el.summary_short}</blockquote>
+    </div>)
+  })}</div>);
+} 
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
